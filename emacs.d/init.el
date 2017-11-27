afc85c943f72d34bfabef48b1a49a5bd31f14d18
Takahiro Kubota <takahirok30@gmail.com>
2017-11-27 17:52:23 +0900

    modified init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,10 +15,10 @@
 (setq load-path
     (append '("/home/takahiro/.emacs.d/elpa/company-0.9.4/") load-path))
 (require 'company)
-(global-company-mode) ; 全バッファで有効にする 
-(setq company-idle-delay 0) ; デフォルトは0.5
-(setq company-minimum-prefix-length 2) ; デフォルトは4
-(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+(global-company-mode) ;enable in all buffer.
+(setq company-idle-delay 0) ;default=0.5
+(setq company-minimum-prefix-length 2) ;default=4
+(setq company-selection-wrap-around t) ;wrap around.
 
 ;yatex
 (setq load-path
